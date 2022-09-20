ead32bfa57bd2d4b3e5a9115e8363ee29b062d6a
YankovichKirill <kirill.best97@gmail.com>
2022-09-20 21:28:00 +0300

    Save3

---
+++ Example3/Program.cs
@@ -0,0 +1,20 @@
+﻿int m = InputNumber("Enter m: ");
+int n = InputNumber("Enter n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Akkermann function = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
