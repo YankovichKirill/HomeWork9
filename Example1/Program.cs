34081a546a8f44768ae4cef560d7a7a86dc27d47
YankovichKirill <kirill.best97@gmail.com>
2022-09-20 20:11:00 +0300

    Save1

---
+++ Example1/Program.cs
@@ -0,0 +1,16 @@
+﻿string SequenceSegmentRec(int M, int N)
+{
+    if (N== M - 1) return "";
+    return SequenceSegmentRec(M, N-1) + "  " + Convert.ToString(N);
+}
+try
+{
+    Console.WriteLine("Enter natural numbers");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SequenceSegmentRec(m, n));
+}
+catch
+{
+    Console.WriteLine("Enter a natural number");
+}
